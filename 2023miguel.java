fc567a5dfa64ec7bac48a40aefd314194cff6190
ronnyvan <115511513+ronnyvan@users.noreply.github.com>
2023-10-02 23:43:11 -0500

    Update 2023miguel.java
    made it more concise but less readable

--- 2023miguel.java
+++ 2023miguel.java
@@ -2,36 +2,32 @@ import java.util.*;
 import java.io.*;
 
 public class App {
-    public static void main(String[] args) throws Exception {
-        Scanner f = new Scanner(new File("miguel.dat"));
-        int n = f.nextInt();
-        f.nextLine();
-        while (n-- > 0) {
-            ArrayList<Character> chars = new ArrayList<>();
-            String s = f.nextLine();
-            
-            for (int i = 0; i < s.length(); i++) {
-                Character c = s.charAt(i);
-                if (Character.isAlphabetic(c)) {
-                    chars.add(c);
-                }
-            }
+  public static void main(String[] args) throws Exception {
+    Scanner f = new Scanner(new File("miguel.dat"));
+    int n = f.nextInt();
+    f.nextLine();
+    while(n-- > 0){
+      String s = f.nextLine();
+      ArrayList<Character> chars = new ArrayList<>();
 
-            Collections.sort(chars, (a, b) -> Character.toLowerCase(a) - Character.toLowerCase(b));
-            String ret = "";
-            for (int i = 0, j = 0;  i < s.length(); i++) {
-                char c = s.charAt(i);
-                if (Character.isAlphabetic(c)) {
-                    if (Character.isUpperCase(c)) {
-                        ret += Character.toUpperCase(chars.get(j++));
-                    } else {
-                        ret += Character.toLowerCase(chars.get(j++));
-                    }
-                } else {
-                    ret += c;
-                }
-            }
-            System.out.println(ret);
+      for(int i = 0; i < s.length(); i++){
+        char currentCharacter = s.charAt(i);
+        if(Character.isAlphabetic(currentCharacter)) chars.add(currentCharacter);
+      }
+
+      Collections.sort(chars, 
+      (c1, c2) -> Integer.compare(Character.toLowerCase(c1), Character.toLowerCase(c2)));
+
+      String ret = "";
+      for(int i = 0, j = 0; i < s.length(); i++){
+        char currentCharacter = s.charAt(i);
+        if(Character.isAlphabetic(currentCharacter)) {
+          if(Character.isUpperCase(currentCharacter)) ret += Character.toUpperCase(chars.get(j++));
+          else ret += Character.toLowerCase(chars.get(j++));
         }
+        else ret += currentCharacter;
+      }
+      System.out.println(ret);
     }
+  }
 }
